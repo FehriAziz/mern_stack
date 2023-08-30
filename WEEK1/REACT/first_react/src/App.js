c41e240a8ce7d31fa957db042544482fe2b4ce30
FehriAziz <azizfehri1@gmail.com>
2023-08-30 14:43:06 +0100

    first_react

---
+++ WEEK1/REACT/first_react/src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+
+function App() {
+
+
+  
+  return (
+<>
+    <div className="App">
+
+      <h1> Hello From App.js😉</h1>
+      <h2>Things i need to do :</h2>
+    </div>
+
+      
+    <div>
+      <ul>
+        <li>learn react</li>
+        <li>climb Mt Everest</li>
+        <li>run a marathon</li>
+        <li>feed the dogs</li>
+      </ul>
+    </div>
+
+
+      
+      
+    
+</>
+    
+  );
+}
+
+export default App;
